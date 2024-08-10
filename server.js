9818866ea302af602d2f025d64d8ebd341cfb3ec
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2024-08-10 09:44:19 +0530

    add voting app

---
+++ server.js
@@ -0,0 +1,9 @@
+// const express = require('express')
+// const app = express();
+// const db = require('./db');
+// require('dotenv').config();
+
+
+// const bodyParser = require('body-parser'); 
+// app.use(bodyParser.json()); // req.body
+// const PORT = process.env.PORT || 3000;
