98b6b3bc2f48b741b275bd385edfa1d8f799567f
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2024-08-10 15:32:41 +0530

    define user and candidate model

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// const express = require('express')
-// const app = express();
+const express = require('express')
+const app = express();
 // const db = require('./db');
 // require('dotenv').config();
 
 
-// const bodyParser = require('body-parser'); 
-// app.use(bodyParser.json()); // req.body
-// const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
