9948d022c82b5e7c19ffdf1ca382b9bc4371e0a7
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2024-08-10 15:37:59 +0530

    Adding password functionality inside user model

--- model/candidate.js
+++ model/candidate.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 // Define the Person schema
 const candidateSchema = new mongoose.Schema({
--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 // Define the Person schema
 const userSchema = new mongoose.Schema({
@@ -62,7 +62,15 @@ userSchema.pre('save', async function(next){
     }
 })
 
-
+userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
