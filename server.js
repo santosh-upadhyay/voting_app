77cfc0f5352a71e3851c139e838fb9ddfc7572f2
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2024-08-10 19:25:51 +0530

    adding login logout signup jwt and all

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
 // Import the router files
-// const userRoutes = require('./routes/userRoutes');
-// const candidateRoutes = require('./routes/candidateRoutes.js');
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes.js');
 
 // Use the routers
-// app.use('/user', userRoutes);
-// app.use('/candidate', candidateRoutes);
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
 
 app.listen(PORT, ()=>{
     console.log('listening on port 3000');
